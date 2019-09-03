6e5282a36257954e9e000cf4414fe440e778fd32
Anatoli Nicolae <desk@anatolinicolae.com>
2019-09-03 16:28:43 +0200

    Added before body process filter
    Signed-off-by: Anatoli Nicolae <desk@anatolinicolae.com>

--- src/plugin.php
+++ src/plugin.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: MailPoet WPML fixture
- * Description: MailPoet WPML fixture implementing mailpoet_before_confirmation_send filter.
+ * Description: MailPoet WPML fixture implementing some filters.
  * Version: PLUGIN_VERSION
  * Author: thundersquared
  * Author URI: https://thundersquared.com/
@@ -40,15 +40,29 @@ class MailPoetWPML
         $file = plugin_dir_path(__FILE__) . '../mailpoet/lib/Subscribers/ConfirmationEmailMailer.php';
         $data = file_get_contents($file);
 
+        // Make safety copy
+        copy($file, "$file.old");
+
         // Check if already fixed
         if (preg_match('/\\$email = apply_filters\\(\'mailpoet_before_confirmation_send\', \\$email\\);/ui', $data)
             == 0)
         {
-            // Make safety copy
-            copy($file, "$file.old");
-
             // Apply fixture
             $result = preg_replace('/\/\/ send email\\n    try {/ui', "\$email = apply_filters('mailpoet_before_confirmation_send', \$email);\n\n    // send email\n    try {", $data);
+
+            // Save file
+            file_put_contents($file, $result);
+        }
+
+        $file = plugin_dir_path(__FILE__) . '../mailpoet/lib/Subscribers/ConfirmationEmailMailer.php';
+        $data = file_get_contents($file);
+
+        // Check if already fixed
+        if (preg_match('/\\$body = apply_filters\\(\'mailpoet_before_body_process\', \\$body\\);/ui', $data)
+            == 0)
+        {
+            // Apply fixture
+            $result = preg_replace('/\/\/ replace list of segments shortcode/ui', "\$body = apply_filters('mailpoet_before_body_process', \$body);\n\n    // replace list of segments shortcode", $data);
 
             // Save file
             file_put_contents($file, $result);
